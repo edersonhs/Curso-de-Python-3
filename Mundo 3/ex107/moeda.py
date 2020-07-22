0a9f88bee7318d053250151c963023a646f77df5
Ederson Henrique Sá <edersonhs3217@gmail.com>
2020-07-21 21:39:46 -0300

    ex107 finalizado

---
+++ Mundo 3/ex107/moeda.py
@@ -0,0 +1,14 @@
+def aumentar(valor, porcentagem):
+    return ((100 + porcentagem) / 100) * valor
+
+
+def diminuir(valor, porcentagem):
+    return ((100 - porcentagem) / 100) * valor
+
+
+def dobro(valor):
+    return valor * 2
+
+
+def metade(valor):
+    return valor / 2
